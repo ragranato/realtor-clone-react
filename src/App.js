cbc4eb6f837580a7fe494571745c07cb79beea54
ragranato <robert.granato@gmail.com>
2023-08-23 22:10:30 -0400

    installed react-router-dom, added routes structure.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
-
-
+import { BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import Home from './pages/Home';
 
 function App() {
   return (
     <div className="App">
-      <h1 className="text-2xl">Hello World!</h1>
+      <Router>
+        <Routes>
+          <Route path='/' element={<Home />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
